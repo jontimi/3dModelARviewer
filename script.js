0b6f5f7ac9fe40271d5119de2cf752dc0b5c65df
jontimi <jon.timi@gmail.com>
2025-07-27 21:13:47 +0300

    Saving local changes before pulling

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function() {
         },
         'tudo': {
             logo: 'tudo-logo.png',
-            website: 'https://www.tudo.co.il/product/%D7%A1%D7%A4%D7%A8%D7%99%D7%94-%D7%9B%D7%95%D7%95%D7%A8%D7%AA-%D7%96%D7%99%D7%92%D7%96%D7%92-%D7%91%D7%A2%D7%99%D7%A6%D7%95%D7%91-%D7%9E%D7%95%D7%93%D7%A8%D7%A0%D7%99-%D7%98%D7%95%D7%93%D7%95-%D7%93%D7%99%D7%96%D7%99%D7%99%D7%9F-%D7%93%D7%92%D7%9d-%D7%95%D7%A8%D7%93-0',
+            website: 'https://www.tudo.co.il/product/%D7%A1%D7%A4%D7%A8%D7%99%D7%94-%D7%9B%D7%95%D7%95%D7%A8%D7%AA-%D7%96%D7%99%D7%92%D7%96%D7%92-%D7%91%D7%A2%D7%99%D7%A6%D7%95%D7%91-%D7%9E%D7%95%D7%93%D7%A8%D7%A0%D7%99-%D7%98%D7%95%D7%93%D7%95-%D7%93%D7%99%D7%96%D7%99%D7%99%D7%9F-%D7%93%D7%92%D7%9D-%D7%95%D7%A8%D7%93-0',
             model: 'HiveShelf90cm.glb',
             dimensionsText: {
                 en: 'Dimensions: W 90cm x H 160cm x D 20cm',
